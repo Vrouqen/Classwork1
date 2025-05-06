cc1717c2fa8c39f7de6bc68b6e272584fa84e095
Vrouqen <108952975+Vrouqen@users.noreply.github.com>
2025-05-06 12:19:04 -0500

    Pequeña modificacion de variables de entorno

--- app.py
+++ app.py
@@ -1,31 +1,20 @@
+import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-
 @app.route("/", methods=['GET'])
 def load_user():
-    datos =[
-        {'id':1,
-         'name':'Vrouqen',
-         'description':'Me gusta programar'
-         },
-         {'id':2,
-         'name':'Nico',
-         'description':'Hola'
-         },
-         {'id':3,
-         'name':'Bryan',
-         'description':'Hola como estás'
-         },
-         {'id':4,
-         'name':'Derek',
-         'description':'No confíen en mí'
-         }
+    datos = [
+        {'id': 1, 'name': 'Vrouqen', 'description': 'Me gusta programar'},
+        {'id': 2, 'name': 'Nico', 'description': 'Hola'},
+        {'id': 3, 'name': 'Bryan', 'description': 'Hola como estás'},
+        {'id': 4, 'name': 'Derek', 'description': 'No confíen en mí'}
     ]
-    return jsonify({'result':datos})
+    return jsonify({'result': datos})
 
-if __name__=="__main__":
-    app.run(debug=True, host="0.0.0.0", port="666")
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
